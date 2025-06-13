418846bf1c25d22fe03476899c8376b8e7147b61
S SIDHARTH <sidharthpunalur@gmail.com>
2025-06-13 20:50:34 +0530

    snap scroll added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react'
+import React from 'react';
 import Navbar from './components/navbar';
 import Hero from './components/landing/hero';
 import Elephant from './components/landing/elephant';
@@ -11,21 +11,37 @@ import Programe from './components/landing/programe';
 import Work from './components/landing/work';
 import Interest from './components/landing/interest';
 
-
 function Home() {
   return (
-    <div>
-      <Navbar/>
-          <Hero/>
-          <Elephant/>
-          <Work/>
-          <Interest/>
-          <Programe/>
-          <Partnership/>
-          <Testimonial/>
-          <Footer/>
+    <div className="h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth">
+      <Navbar />
+      
+      <div className="snap-start">
+        <Hero />
+      </div>
+      <div className="snap-start">
+        <Elephant />
+      </div>
+      <div className="snap-start">
+        <Work />
+      </div>
+      <div className="snap-start">
+        <Interest />
+      </div>
+      <div className="snap-start">
+        <Programe />
+      </div>
+      <div className="snap-start">
+        <Partnership />
+      </div>
+      <div className="snap-start">
+        <Testimonial />
+      </div>
+      <div className="snap-start">
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
