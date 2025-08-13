e052aa7ea2e022b6807c6a358c7f4dd34f9f4524
Renjith312 <darkknight312137@gmail.com>
2025-08-13 21:33:30 +0530

    footer

--- src/app/components/footerblank.jsx
+++ src/app/components/footerblank.jsx
@@ -36,16 +36,19 @@ const Footer = () => {
 
       {/* Bottom Row */}
       <div className="relative z-20 mt-10 flex flex-col md:flex-row justify-between items-center text-xs sm:text-sm text-gray-500 gap-4">
-      <Link href="/term" className="text-sm text-black hover:text-white transition-colors">
-         Privacy Policy | Terms & Conditions
-    </Link>
-    <div className="flex flex-column md:flex-row justify-center md:justify-start items-center gap-4 text-xl text-gray-700">
+        <Link href="/term" className="text-sm text-black hover:text-white transition-colors">
+           Privacy Policy | Terms & Conditions
+        </Link>
+        
+        {/* Centered Social Icons */}
+        <div className="flex justify-center items-center gap-4 text-xl text-gray-700 md:absolute md:left-1/2 md:transform md:-translate-x-1/2">
           <a href="#"><FaTwitter /></a>
           <a href="#"><FaLinkedin /></a>
           <a href="#"><FaYoutube /></a>
           <a href="#"><FaFacebook /></a>
           <a href="#"><FaInstagram /></a>
         </div>
+        
         <p className="text-lg text-black sm:text-xl">{new Date().getFullYear()}</p>
       </div>
     </footer>
