9d3090b574d9529030dbfe645ad31ebd3a82a5ba
Renjith312 <darkknight312137@gmail.com>
2025-08-15 12:18:08 +0530

    footer

--- src/app/components/profooterblank.jsx
+++ src/app/components/profooterblank.jsx
@@ -11,7 +11,7 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="relative text-gray-800 px-6 sm:px-10 md:px-20 overflow-hidden bg-transparent md:h-[400px] flex flex-col justify-end  pb-8">
+    <footer className="relative text-gray-800 px-6 sm:px-10 md:px-20 overflow-hidden bg-transparent md:h-[400px] flex flex-col justify-end pb-8">
       {/* Decorative Half Circle */}
       <div className="absolute bottom-0 left-0 z-0 w-[1200px] h-[900px] overflow-hidden pointer-events-none">
         <Image
@@ -36,16 +36,19 @@ const Footer = () => {
 
       {/* Bottom Row */}
       <div className="relative z-20 mt-10 flex flex-col md:flex-row justify-between items-center text-xs sm:text-sm text-gray-500 gap-4">
-      <Link href="/term" className="text-sm text-white hover:text-white transition-colors">
-         Privacy Policy | Terms & Conditions
-    </Link>
-    <div className="flex flex-column md:flex-row justify-center md:justify-start items-center gap-4 text-xl text-white">
-    <a href="#"><FaTwitter /></a>
+        <Link href="/term" className="text-sm text-white hover:text-white transition-colors">
+           Privacy Policy | Terms & Conditions
+        </Link>
+        
+        {/* Centered Social Icons */}
+        <div className="flex justify-center items-center gap-4 text-xl text-white md:absolute md:left-1/2 md:transform md:-translate-x-1/2">
+        <a href="#"><FaTwitter /></a>
           <a href="https://www.linkedin.com/company/histare/"><FaLinkedin /></a>
           <a href="https://www.youtube.com/@thehistaregroup7950"><FaYoutube /></a>
           <a href="#"><FaFacebook /></a>
           <a href="https://www.instagram.com/histare.concepts/"><FaInstagram /></a>
         </div>
+        
         <p className="text-lg text-white sm:text-xl">{new Date().getFullYear()}</p>
       </div>
     </footer>
