36aabaf76f118fc5dcb466a992b5b04932031949
ashvin <ashvinpkumar2004@gmail.com>
2025-08-22 21:27:26 +0530

    ss

--- src/app/components/landing/programe.jsx
+++ src/app/components/landing/programe.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 function Programe() {
   return (
     <div className="py-16 px-4 md:px-20 pb-10 text-center min-h-screen flex flex-col justify-center">
-      <div className="rounded-xl overflow-hidden pt-5 mb-10">
+      <div className="rounded-xl overflow-hidden pt-5 mb-1 md:mb-10">
         <Image
           src="/images/art/rang.svg"
           alt="Collectors' Affair"
@@ -13,7 +13,7 @@ function Programe() {
           className="object-contain w-full h-[35px] sm:h-[45px] md:h-[70px] lg:h-[80px]"
         />
       </div>
-
+       
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 justify-center items-center pt-10 md:pt-0">
         <a href="/collectors">
           <div className="rounded-xl overflow-hidden aspect-[3/4] bg-[#2B3730]">
@@ -26,7 +26,7 @@ function Programe() {
             />
           </div>
         </a>
-
+         
         <a href='/erbe'>
           <div className="rounded-xl overflow-hidden aspect-[3/4] bg-[#6B2134]">
             <Image
@@ -38,7 +38,7 @@ function Programe() {
             />
           </div>
         </a>
-        
+                 
         <a href='/vitrine'>
           <div className="rounded-xl overflow-hidden aspect-[3/4] bg-[#0E2B4C]">
             <Image
@@ -50,7 +50,7 @@ function Programe() {
             />
           </div>
         </a>
-        
+                 
         <a href='/now'>
           <div className="rounded-xl overflow-hidden aspect-[3/4] bg-[#FFFFFF]">
             <Image
