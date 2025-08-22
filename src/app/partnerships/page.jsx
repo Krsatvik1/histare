5e9eefc39b910059c927749c1ee9db78c4ee014e
navadeep satheesh <navadeepsatheesh@gmail.com>
2025-08-22 19:19:28 +0530

    f

--- src/app/partnerships/page.jsx
+++ src/app/partnerships/page.jsx
@@ -13,10 +13,11 @@ export default function Partnerships() {
       <section className="snap-start snap-always min-h-screen flex items-center justify-center px-6 md:px-20">
         <h2
           className="text-4xl md:text-5xl text-[#3c597B] text-center"
-          style={{ fontFamily: 'Rofane' }}
+          style={{ fontFamily: 'Playfair Display' }}
         >
-          <span className="italic font-normal">The </span>Value
-          <span className="italic font-normal"> of </span>Partnerships
+          <span className="italic font-normal" style={{ fontFamily: 'Rofane' }}>The </span>Value
+          <span className="italic font-normal " style={{ fontFamily: 'Rofane' }}> of </span>
+          <span style={{ fontFamily: 'Rofane' }}>Partnerships</span>
         </h2>
       </section>
 
