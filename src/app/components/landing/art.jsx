2224af65cf5c06f8ac777ed5b495934c500eb622
navadeep satheesh <navadeepsatheesh@gmail.com>
2025-09-22 21:04:11 +0530

    f

--- src/app/components/landing/art.jsx
+++ src/app/components/landing/art.jsx
@@ -533,7 +533,7 @@ export default function Work() {
         />
       </div>
 
-      <div className="flex-1 flex items-center !h-24  bg-green-200 !mt-0" style={{fontFamily:'Optima'}}>
+      <div className="flex-1 flex items-center !h-24  -bg-green-200 !mt-0" style={{fontFamily:'Optima'}}>
         <div 
           className="work-scroll-container bg-blue-200"
           onMouseEnter={handleMouseEnter}
