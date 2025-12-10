91bfd3c521c0dce646bc8f76390dd0394aafcd13
ashvin <ashvinpkumar2004@gmail.com>
2025-12-10 15:29:17 +0530

    version updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['dev-histare.netlify.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dev-histare.netlify.app',
+      },
+    ],
   },
 };
 
